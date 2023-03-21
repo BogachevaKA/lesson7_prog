37a6b4fcc332b6952505b8aadaaa8a7c8ff2b7a3
BogachevaKA1 <BogachevaKA1@it.mos.ru>
2023-03-21 23:26:21 +0300

    exercise16

--- exercise16/Program.cs
+++ exercise16/Program.cs
@@ -35,6 +35,7 @@ double[] SumArray(int[,] array1)
     for (int i = 0; i < newArray.Length; i++)
     {
         newArray[i] /= array1.GetLength(0);
+    Console.WriteLine($"Среднее арифметическое № {i+1} равно {newArray}");
     }
     return newArray;
 }
@@ -47,5 +48,4 @@ int cols = Convert.ToInt32(Console.ReadLine());
 int[,] array = new int[rows, cols];
 FillArray(array);
 PrintArray(array);
-Console.WriteLine;
-PrintArray(SumArray(array));
+SumArray(array);
